17d89a8acd91c7d5ff8f612ff268a6c706483773
damsara-ekanayake <damsaraudana180@gmail.com>
2024-06-22 13:21:36 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool isExiting = false;
+
+do
+{
+    Console.WriteLine("Hello!");
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+
+    var userInput = Console.ReadLine();
+
+    switch (userInput)
+    {
+        case "S":
+        case "s":
+            Console.WriteLine("\n See all \n");
+            break;
+
+        case "A":
+        case "a":
+            Console.WriteLine("\n add memo \n");
+            break;
+
+        case "R":
+        case "r":
+            Console.WriteLine("\n remove memo \n ");
+            break;
+
+        case "E":
+        case "e":
+            isExiting = true;
+            break;
+
+        default:
+            Console.WriteLine("\n Invalid Input! \n");
+            break;
+    }
+
+}
+while (!isExiting);
+
+
+
